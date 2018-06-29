072d4b7bf8e7da6702331e1db2c331747a88810b
Administrator <administrator@gbtc408013dt.montgomerycollege.edu>
2018-06-29 14:08:41 -0400

    Initial Commit

---
+++ ParseStarterProject/Contact.swift
@@ -0,0 +1,20 @@
+//
+//  Contacts.swift
+//  Oracao
+//
+//  Created by Administrator on 6/29/18.
+//  Copyright © 2018 Administrator. All rights reserved.
+//
+
+import Foundation
+
+class Contact {
+    var id: String
+    var name: String
+    
+    init(id: String, name: String) {
+        self.id = id
+        self.name = name
+    }
+}
+
